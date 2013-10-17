4a625c1988e0749b866baf60ebea6078719fa33b
Bart Vangeneugden <bart@bartv.be>
2013-10-17 16:51:17 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var jsdom = require('jsdom');
+
+var fs = require('fs');
+
+function findContent(search) {
+	var result = new Array();
+	fs.readdir('/Users/tonchen/Desktop/express/', function(err, files) {
+    	files.filter(function(file) { return file.substr(-5) == '.html'; })
+         .forEach(function(file) { 
+			 fs.readFile('/Users/tonchen/Desktop/express/' + file, 'utf-8', function(err, contents) { result.push(inspectFile(contents, err, search)); }); 
+		 });
+	 });
+	 console.info("aaaaa" + result);
+	return result;
+}
+
+function inspectFile(contents, err, search) {
+	if (contents.indexOf(search) != -1) {
+		console.info("found");
+		return "fo";
+	}
+}
+
+app.get('/hello.txt', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  body += findContent("ignoredResultNode");
+  res.end(body);
+});
+app.listen(3000);
+console.log('Listening on port 3000');
