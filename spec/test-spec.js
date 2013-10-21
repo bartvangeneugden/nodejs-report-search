bedc3d7b278022c0cac87a40efa427c5069898ea
Bart Vangeneugden <bart@bartv.be>
2013-10-21 12:30:47 +0100

    Remove unnecessary files

--- spec/test-spec.js
+++
@@ -1,7 +0,0 @@
-var utils = require('../app/util/utils.js')
-describe('jasmine-node', function(){
-	it("Should Pass basic arithmic", function(){
-		expect(utils.testAddMe(1,2)).toEqual(3);
-		expect(utils.testMultiplyMe(3, 2)).toEqual(6)
-	})
-});
