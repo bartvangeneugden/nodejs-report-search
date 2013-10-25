af2b4eff45f6f15c4dab7cd9297b67bb2f72452e
Bart Vangeneugden <bart@bartv.be>
2013-10-25 15:21:47 +0100

    Happy path web service

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var app = express();
 var jsdom = require('jsdom');
-var IOHandler = require('../app/util/iohandler.js');
-var ScalaReader = require('../app/util/scalatest.js');
+var IOHandler = require('./app/util/iohandler.js');
+var ScalaReader = require('./app/util/scalatest.js');
 
 app.get('/scala.json', function(req, response){
-	IOHandler.foreachFileIn("./spec/testresources", function(contents){
-		var scenario = ScalaReader.getScenario(contents);
-		if(ScalaReader.scenarioContains(scenario, contents)){
-			return scenario;
-		}else{
-			return false;
+	IOHandler.foreachFileIn(
+		"./spec/testresources", 
+		function(contents){
+			var scenario = ScalaReader.getScenario(contents);
+			if(ScalaReader.scenarioContainsKeyword(scenario, req.query.q)){
+				return scenario;
+			}else{
+				return false;
+			}
+		},
+		function(results){
+			response.json(results);
 		}
-	}, function(results){
-		console.log(results);
-	}));
-	req.query.q
+	);
 });
 
 app.listen(3000);
